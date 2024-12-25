b789cbb7ff0fe51dc20ec6769bce0775f60f200b
yuberee <59583743+yuberee@users.noreply.github.com>
2024-12-25 16:15:29 +0100

    Main scene, rotate world speed

--- code/ChristmassyGameLogic.cs
+++ code/ChristmassyGameLogic.cs
@@ -2,8 +2,24 @@ using Sandbox;
 
 public sealed class ChristmassyGameLogic : Component
 {
+	/// <summary>
+	/// The map we're going to rotate
+	/// </summary>
+	[Property]
+	public GameObject Map { get; set; }
+
+	/// <summary>
+	/// How many degrees it rotates per second
+	/// </summary>
+	[Property]
+	[Range( 1f, 100f, 1f )]
+	public float RotationSpeed { get; set; } = 5f;
+
+
 	protected override void OnUpdate()
 	{
+		if ( !Map.IsValid() ) return;
 
+		Map.WorldRotation *= Rotation.FromPitch( RotationSpeed * Time.Delta );
 	}
 }
